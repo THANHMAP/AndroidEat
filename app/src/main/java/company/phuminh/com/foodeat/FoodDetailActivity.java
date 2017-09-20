04284e6e627a2badf94e634356ef119d02948a32
Thanh Nguyen <thanh.nguyen@mtech.com.vn>
2017-09-20 14:49:07 +0700

    no message

---
+++ app/src/main/java/company/phuminh/com/foodeat/FoodDetailActivity.java
@@ -0,0 +1,13 @@
+package company.phuminh.com.foodeat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoodDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_detail);
+    }
+}
